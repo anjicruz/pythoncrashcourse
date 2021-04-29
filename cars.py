65dfd60ac27d513e858053b1872e768124f9a0d8
anjicruz <anji_cruz@msn.com>
2021-04-29 12:27:49 +0800

    A simple if example 0429 1230PM

--- cars.py
+++ cars.py
@@ -1,6 +1,16 @@
+# A simple if example
 cars=['bmw','audi','toyota','subaru']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+
+
 # len(cars)
-print(cars[-1])
+# print(cars[-1])
 
 #print("Here is the original list:")
 #print(cars)
@@ -9,3 +19,4 @@ print(cars[-1])
 #print("\nHere is the original list again:")
 
 # cars.sort(reverse=True)
+
