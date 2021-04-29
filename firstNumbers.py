bd3baba5d37f6ddbe355899e098a4dc23ca96c3b
anjicruz <anji_cruz@msn.com>
2021-04-29 09:09:10 +0800

    slicing a list / Copying a List 0429 900AM

--- firstNumbers.py
+++ firstNumbers.py
@@ -15,3 +15,5 @@ print(squares)
 
 squares=[value**2 for value in range(1,11)]
 print(squares)
+
+                        
---
+++ players.py
@@ -0,0 +1,23 @@
+# slicing a list / Copying a List
+myFoods = ['pizza','falafel','carrot cake']
+#friendFood = myFoods[:]
+friendFood = myFoods
+myFoods.append('canoli')
+friendFood.append('ice cream')
+print("My favorite foods are:")
+print(myFoods)
+print("\nMy friend's favorite foods are:")
+print(friendFood)
+
+
+#players = ['charles','martina','michael','florence','eli']
+#print("Here are the first three players on my team:")
+#for player in players[:3]:
+#    print(player.title())
+
+#print(players[0:3])
+#print(players[1:4])
+#print(players[:4]) 
+#print(players[2:])
+#print(players[-3])
+
