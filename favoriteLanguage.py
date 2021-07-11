99dc53b5843b0bfc84b872189d1878eb20f4f8c9
anjicruz <anji_cruz@msn.com>
2021-07-11 15:22:01 +0800

    commit

--- favoriteLanguage.py
+++
@@ -1,8 +0,0 @@
-# favoriteLanguage=' python   '
-# print(favoriteLanguage.rstrip())
-# print(favoriteLanguage.lstrip())
-# message = 'One of Python's strength is its diverse community'
-# print(message)
-sum = 0.001 + 0.12
-print(sum)
-sum=3+2
--- full_name.py
+++
@@ -1,7 +0,0 @@
-firstName = "ada"
-lastName = "lovelace"
-fullName = f"{firstName} {lastName}"
-# print(f"Hello {fullName.title()}!")
-
-message=f"Hello {fullName.title()}!"
-print(message)
